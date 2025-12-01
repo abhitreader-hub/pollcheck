49e723c0a439098f2b3396294c2e822b687f993b
abhitreader-hub <abhitreader@gmail.com>
2025-12-01 14:20:57 +0530

    Update voters.ts

--- services/voters.ts
+++ services/voters.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:4000';
+const API_URL = 'https://pollcheck-backend.uc.r.appspot.com/';
 
 function getToken(): string | null {
   return localStorage.getItem('token');
